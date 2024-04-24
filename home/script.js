d07a908ce1b86b43842796e9ac388abab04ba692
Daniele De Pascali <danieledp02@gmail.com>
2024-04-24 20:52:46 +0200

    adjusted scroll nav-bar and added button for mobile

--- home/script.js
+++ home/script.js
@@ -4,6 +4,7 @@ const body = document.querySelector("body"),
       modeSwitch = document.querySelector(".toggle-switch"),
       modeText = document.querySelector(".mode-text"),
       mobile_toggle = document.querySelector(".mobile-toggle");
+      exit_button= document.querySelector(".exitbutton");
 
 const hideSidebar = () => {
     sidebar.classList.add("close");
@@ -15,11 +16,12 @@ const showSidebar = () => {
 mobile_toggle.addEventListener("click", (e) => {
     if (sidebar.classList.contains("close")) {
         sidebar.classList.remove("close");
-    } else {
-        sidebar.classList.add("close");
-    }
+    } 
 });
 
+exit_button.addEventListener("click", (e) => {
+        sidebar.classList.add("close");
+});
 sidebar.addEventListener("mouseleave", hideSidebar);
 sidebar.addEventListener("mouseenter", showSidebar);
 

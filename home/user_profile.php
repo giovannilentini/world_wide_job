bbd960b8789a73db223ddfe7fcdcbd283b6d6fe1
giovannilentini <giovannilentini29@gmail.com>
2024-05-14 15:41:30 +0200

    cleaning

--- home/user_profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if ($_POST['other_user_id'] == $_SESSION['session_id']) {
-        header("refresh:0;url=../profile/profile.php");
-    } else {
-        $_SESSION['session_visit'] = $_POST['other_user_id'];
-        header("refresh:0;url=../other_user/profile.php");
-    }
-?>
